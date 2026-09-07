c65950b7e877ae2b8a61210b6af7340cc5c35c15
Stephen Olesen <slepp@slepp.ca>
2026-09-07 16:28:22 -0600

    chore(cabi): regenerate the surface header for the time exports

--- hew-cabi/include/hew_cabi_surface.h
+++ hew-cabi/include/hew_cabi_surface.h
@@ -16,10 +16,10 @@ struct hew_cabi_manifest_row {
   enum hew_cabi_manifest_ownership ownership;
 };
 
-#define HEW_CABI_MANIFEST_FUNCTION_COUNT 1622u
+#define HEW_CABI_MANIFEST_FUNCTION_COUNT 1623u
 #define HEW_CABI_MANIFEST_STATIC_COUNT 24u
 
-static const struct hew_cabi_manifest_row hew_cabi_manifest_functions[1622] = {
+static const struct hew_cabi_manifest_row hew_cabi_manifest_functions[1623] = {
     {"hew_actor_ask",
      "{\"native\": \"fn hew_actor_ask( *mut HewActor, i32, *mut c_void, usize, "
      ") -> *mut c_void\", \"wasm32-wasip1\": \"fn hew_actor_ask( *mut "
